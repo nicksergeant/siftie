9dd0131b6f4193a676d4a4acfbff2786d6e58d94
Nick Sergeant <nick@nicksergeant.com>
2017-05-15 15:20:44 -0400

    Just post directly.

--- server/slack.jsx
+++ server/slack.jsx
@@ -17,36 +17,13 @@ const hasProp = {}.hasOwnProperty;
 Slack = {
   notify: function(text, slackWebhookUrl) {
     if (Meteor.absoluteUrl() === 'http://siftie.com/') {
-      return Job.push(new SlackJob({
-        text: text,
-        slackWebhookUrl: slackWebhookUrl
-      }));
+      HTTP.post(slackWebhookUrl, {
+        data: {
+          username: 'Siftie',
+          text: text,
+          icon_url: 'http://i.nick.sg/image/0o433R110Q2a/mGM0x2RQ.png'
+        }
+      });
     }
   }
 };
-
-SlackJob = (function(superClass) {
-  extend(SlackJob, superClass);
-
-  function SlackJob() {
-    return SlackJob.__super__.constructor.apply(this, arguments);
-  }
-
-  SlackJob.prototype.handleJob = function() {
-    return HTTP.post(this.params.slackWebhookUrl, {
-      data: {
-        username: 'Siftie',
-        text: this.params.text,
-        icon_url: 'http://i.nick.sg/image/0o433R110Q2a/mGM0x2RQ.png'
-      }
-    });
-  };
-
-  SlackJob.prototype.afterJob = function(error) {
-    if (error) {
-      RavenLogger.log(error);
-    }
-  };
-
-  return SlackJob;
-})(Job);
