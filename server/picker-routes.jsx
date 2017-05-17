9a3ac6a9c4827a63e585bfcf99ea3c8294d83426
Nick Sergeant <nick@nicksergeant.com>
2017-05-17 15:36:35 -0400

    Active feed RSS and fix date sort on curated feed.

--- server/picker-routes.jsx
+++ server/picker-routes.jsx
@@ -1,9 +1,51 @@
+Picker.route('/:teamSlug/active/rss', function(params, req, res, next) {
+  const team = Teams.findOne({ slug: params.teamSlug });
+
+  if (!team) return res.end();
+
+  const teamItems = TeamItems.find({ teamId: team._id }, {
+    sort: { lastActiveDate: -1 }
+  }).fetch();
+
+  const items = [];
+
+  teamItems.forEach((teamItem) => {
+    items.push(Items.findOne({ _id: teamItem.itemId }));
+  });
+
+  const itemizer = (item) => {
+    return `
+      <item>
+        <title>${item.title}</title>
+        <link>${item.link}</link>
+        <description><![CDATA[${item.description}]]></description>
+        <pubDate>${item.pubDate}</pubDate>
+      </item>
+    `;
+  };
+
+  const itemsXml = items.map((item) => {
+    return itemizer(item);
+  });
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.end(`<?xml version="1.0" encoding="utf-8" ?>
+    <rss version="2.0">
+      <channel>
+        ${itemsXml}
+      </channel>
+    </rss>
+  `);
+});
+
 Picker.route('/:teamSlug/curated/rss', function(params, req, res, next) {
   const team = Teams.findOne({ slug: params.teamSlug });
 
   if (!team) return res.end();
 
-  const items = CuratedItems.find({ teamId: team._id }).fetch();
+  const items = CuratedItems.find({ teamId: team._id }, {
+    sort: { date_published: -1 }
+  }).fetch();
 
   const itemizer = (item) => {
     return `
