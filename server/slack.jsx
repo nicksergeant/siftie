d2a598de449fd0c5ac998347722ec10023c440f8
Nick Sergeant <nsergeant@hubspot.com>
2019-03-07 14:52:08 -0500

    I have no idea what this is.

--- server/slack.jsx
+++ server/slack.jsx
@@ -1,18 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 
-const extend = function(child, parent) {
-  for (var key in parent) {
-    if (hasProp.call(parent, key)) child[key] = parent[key];
-  }
-  function ctor() {
-    this.constructor = child;
-  }
-  ctor.prototype = parent.prototype;
-  child.prototype = new ctor();
-  child.__super__ = parent.prototype;
-  return child;
-};
-const hasProp = {}.hasOwnProperty;
+// const extend = function(child, parent) {
+//   for (var key in parent) {
+//     if (hasProp.call(parent, key)) child[key] = parent[key];
+//   }
+//   function ctor() {
+//     this.constructor = child;
+//   }
+//   ctor.prototype = parent.prototype;
+//   child.prototype = new ctor();
+//   child.__super__ = parent.prototype;
+//   return child;
+// };
+// const hasProp = {}.hasOwnProperty;
 
 Slack = {
   notify: function(text, slackWebhookUrl) {
