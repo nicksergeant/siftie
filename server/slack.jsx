cc89d4f1c0e4d053be8587c68e8ab1973e8f3203
Nick Sergeant <nsergeant@hubspot.com>
2019-03-07 14:58:20 -0500

    Use a modern node.

--- server/slack.jsx
+++ server/slack.jsx
@@ -1,19 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-// const extend = function(child, parent) {
-//   for (var key in parent) {
-//     if (hasProp.call(parent, key)) child[key] = parent[key];
-//   }
-//   function ctor() {
-//     this.constructor = child;
-//   }
-//   ctor.prototype = parent.prototype;
-//   child.prototype = new ctor();
-//   child.__super__ = parent.prototype;
-//   return child;
-// };
-// const hasProp = {}.hasOwnProperty;
-
 Slack = {
   notify: function(text, slackWebhookUrl) {
     if (Meteor.absoluteUrl() === 'https://reader.siftie.com/') {
